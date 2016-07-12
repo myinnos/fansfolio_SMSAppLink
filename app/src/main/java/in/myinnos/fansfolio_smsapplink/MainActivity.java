4c422405f56e11b2590faec038a9cfa94d2d1f4f
Prabhakar Thota <prabhakar_thota@live.com>
2016-07-12 10:36:24 +0530

    fans folio android app!

---
+++ app/src/main/java/in/myinnos/fansfolio_smsapplink/MainActivity.java
@@ -0,0 +1,38 @@
+package in.myinnos.fansfolio_smsapplink;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import in.myinnos.textmeapp_fansfolio.TextMefansfolioApp;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etMobileNumber;
+    private Button btSend;
+    private String mobileNumber;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initializing variables
+        etMobileNumber = (EditText) findViewById(R.id.etMobileNumber);
+        btSend = (Button) findViewById(R.id.btSend);
+
+        // send button on click listener
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mobileNumber = etMobileNumber.getText().toString();
+                //calling library funtion
+                TextMefansfolioApp.make(MainActivity.this, mobileNumber, "one time SMS Sent! to "+ mobileNumber);
+            }
+        });
+    }
+}
